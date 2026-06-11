35c38fe78a74bf747e2dcb712428b859d998536b
Krishna <thekrishnadhami17@gmail.com>
2026-06-11 14:55:35 +0545

    minor change

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:5000/api/:path*",
+        destination:
+          "https://final-final-year-project-backend.onrender.com/api/:path*",
       },
     ];
   },
